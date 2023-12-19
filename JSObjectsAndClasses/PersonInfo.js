5f3286c080ebec91a7f722f9a21c3380e1f044e6
Antoan Furlanski <antoan.furlanski@gmail.com>
2023-12-19 16:54:03 +0200

    Objects and classes

---
+++ JSObjectsAndClasses/PersonInfo.js
@@ -0,0 +1,10 @@
+function createPerson(firstName, lastName, age) {
+    let person = {
+      firstName,
+      lastName,
+      age,
+    };
+  
+    return person;
+  }
+  console.log(createPerson("Peter", "Pan", "20"));
